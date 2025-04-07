612b3321a8d68ed28c8d237e72ca1e40d023d333
Anmol Mathias <anmolmathias@gmail.com>
2025-04-07 12:07:50 +0530

    re-init

--- config/darkman/dark-mode.d/dark.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-cp -rf "$HOME/.config/sway/themes/apprentice-dark.conf" "$HOME/.config/sway/themes/colors.conf"
-sway reload
-sed -i 's/habalbum/habamax/g' /home/nml/.vimrc
-kitten themes --reload-in=all habadark
-gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3-dark' && gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
--- config/darkman/light-mode.d/light.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-cp -rf "$HOME/.config/sway/themes/apprentice-light.conf" "$HOME/.config/sway/themes/colors.conf"
-sway reload
-sed -i 's/habamax/habalbum/g' /home/nml/.vimrc
-kitten themes --reload-in=all habalight
-gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3' && gsettings set org.gnome.desktop.interface color-scheme 'default'
